7f0526390bb306d3c31a672a20c68a5ada11c817
Ashpaque Shaikh <ashfaq664236@gmail.com>
2025-01-28 18:01:13 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="ashpaqueshaikh4236",
+    author_email="ashfaq664236@gmail.com",
+    packages=find_packages()
+)
