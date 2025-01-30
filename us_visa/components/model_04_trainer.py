08d70fec7201667956d5bb046ccef4ea46160083
Ashpaque Shaikh <ashfaq664236@gmail.com>
2025-01-30 18:12:26 +0530

    mlflow Added

--- us_visa/components/model_04_trainer.py
+++ us_visa/components/model_04_trainer.py
@@ -1,6 +1,7 @@
 import sys
 from typing import Tuple
-
+import mlflow
+from urllib.parse import urlparse
 import numpy as np
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from neuro_mf  import ModelFactory
@@ -17,26 +18,54 @@ class ModelTrainer:
         self.data_transformation_artifact = data_transformation_artifact
         self.model_trainer_config = model_trainer_config
 
-    def get_model_object_and_report(self, train: np.array, test: np.array) -> Tuple[object, object]:
-        try:
-            logging.info("Using neuro_mf to get best model object and report")
-            
-            model_factory = ModelFactory(model_config_path=self.model_trainer_config.model_config_file_path)
-            
-            x_train, y_train, x_test, y_test = train[:, :-1], train[:, -1], test[:, :-1], test[:, -1]
 
-            best_model_detail = model_factory.get_best_model(X=x_train,y=y_train,base_accuracy=self.model_trainer_config.expected_accuracy_score_train_data)
-            model_obj = best_model_detail.best_model
 
-            y_pred = model_obj.predict(x_test)
-            
+    def eval_metrics(self,y_test, y_pred):
             accuracy = accuracy_score(y_test, y_pred) 
             f1 = f1_score(y_test, y_pred)  
             precision = precision_score(y_test, y_pred)  
             recall = recall_score(y_test, y_pred)
-            test_data_metric_artifact = ClassificationMetricArtifactTestData(accuracy=accuracy, f1_score=f1, precision_score=precision, recall_score=recall)
+            return accuracy, f1, precision, recall
+
+    def get_model_object_and_report(self, train: np.array, test: np.array) -> Tuple[object, object]:
+        try:
+            logging.info("Using neuro_mf to get best model object and report")
+
+            logging.info("Add metrics in mlfow")
+
+            mlflow.set_registry_uri("DAGSHUB_URL")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            with mlflow.start_run():
             
-            return best_model_detail, test_data_metric_artifact
+                model_factory = ModelFactory(model_config_path=self.model_trainer_config.model_config_file_path)
+                
+                
+                x_train, y_train, x_test, y_test = train[:, :-1], train[:, -1], test[:, :-1], test[:, -1]
+
+                best_model_detail = model_factory.get_best_model(X=x_train,y=y_train,base_accuracy=self.model_trainer_config.expected_accuracy_score_train_data)
+                model_obj = best_model_detail.best_model
+                
+                y_pred = model_obj.predict(x_test)
+
+                (accuracy, f1, precision, recall) = self.eval_metrics(y_test, y_pred)
+            
+                mlflow.log_metric('accuracy', accuracy)
+                mlflow.log_metric('f1', f1)
+                mlflow.log_metric('precision', precision)
+                mlflow.log_metric('recall', recall)
+
+                if tracking_url_type_store != "file":
+
+                    mlflow.sklearn.log_model(model_obj, "model", registered_model_name="best_model")
+                else:
+                    mlflow.sklearn.log_model(model_obj, "model")
+
+                logging.info("End metrics in mlfow")
+
+                test_data_metric_artifact = ClassificationMetricArtifactTestData(accuracy=accuracy, f1_score=f1, precision_score=precision, recall_score=recall)
+            
+                return best_model_detail, test_data_metric_artifact
         
         except Exception as e:
             raise USvisaException(e, sys) from e
