7d0244c7ef63bb55aa10e734d1e96ca387102bff
Ashpaque Shaikh <ashfaq664236@gmail.com>
2025-02-02 23:59:21 +0530

    Terrafrom

---
+++ eks_code/vpc.tf
@@ -0,0 +1,31 @@
+locals {
+  region = "ap-south-1"
+  vpc_cidr = "10.0.0.0/16"
+  azs      = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.13.0"
+
+  name = "My-cluster"
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+  intra_subnets   = local.intra_subnets
+
+  enable_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
