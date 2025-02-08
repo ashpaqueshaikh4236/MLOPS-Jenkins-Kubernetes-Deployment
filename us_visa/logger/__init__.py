042499a78358c2ce45a3282bd92ca9148b3449df
Ashpaque Shaikh <157145377+ashpaqueshaikh4236@users.noreply.github.com>
2025-02-09 04:26:34 +0530

    Update __init__.py

--- us_visa/logger/__init__.py
+++ us_visa/logger/__init__.py
@@ -4,7 +4,7 @@ import os
 from from_root import from_root
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
 
 log_dir = 'logs'
 
@@ -16,7 +16,7 @@ os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
     filename=logs_path,
-    format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
+    format="[ %(asctime)s ] - %(levelname)s - %(message)s",
     level=logging.DEBUG,)
 
 logging.getLogger("pymongo").setLevel(logging.WARNING)
