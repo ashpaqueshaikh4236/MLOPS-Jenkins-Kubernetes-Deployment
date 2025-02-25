950cd71ab15866252d39b1ea583952ade8028b36
Ashpaque Shaikh <157145377+ashpaqueshaikh4236@users.noreply.github.com>
2025-02-25 23:06:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="usvisa",
-    version="0.0.0",
+    version="0.0.1",
     author="ashpaqueshaikh4236",
     author_email="ashfaq664236@gmail.com",
     packages=find_packages()
